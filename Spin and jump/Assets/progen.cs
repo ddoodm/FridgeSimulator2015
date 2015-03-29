42db615952c598701c0e420cd087c4164f6a123a
robafett <ram_95@msn.com>
2015-03-29 17:37:40 +1100

    Pregeneration of Straight and Spinning path
    Known bugs: 'spawning' places of the spinning tiles rotate along with
    the mid tile, this results in odd angles for the path, which results in
    the player running off the edge.
    
    Score still goes down if you turn urself around back to origin.

---
+++ Spin and jump/Assets/progen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class progen : MonoBehaviour {
+
+	public Transform spawner;
+	public GameObject platformStraight;
+	public GameObject platformRotating;
+	public GameObject PlatformCorner;
+
+	void OnTriggerEnter(Collider other)
+	{
+		int choice = Random.Range (0, 2);
+		
+		if (other.tag == "Player") {
+		switch(choice)
+			{
+			case 0:
+				Instantiate (platformStraight,spawner.position,spawner.rotation);
+				break;
+			case 1:
+				Instantiate (platformRotating,spawner.position,spawner.rotation);
+				break;
+
+			default:
+				Instantiate (platformStraight,spawner.position,spawner.rotation);
+				break;
+			//Currently can only spawn straight or spinning platforms no corners
+			/*case 2:
+				Instantiate (PlatformCorner,spawner.position,spawner.rotation);
+				break;*/
+			}
+
+
+		}
+	}
+}
---
+++ Spin and jump/Assets/progenR.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class progenR : MonoBehaviour {
+	
+	public Transform spawnerN;
+	public Transform spawnerE;
+	public Transform spawnerW;
+	public Transform spawnerS;
+	public GameObject platformStraight;
+	public GameObject platformCorner;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		int spawnchoice = Random.Range (0, 1);
+//		int choice = Random.Range (0, 1);  Eventually have it random between either straight or corner
+		int choice = 0;
+//		Debug.Log (choice);
+		if (other.tag == "Player") {
+			switch (spawnchoice)
+			{
+			case 0:
+				switch(choice)
+				{
+				case 0:
+					Instantiate (platformStraight,spawnerN.position,spawnerN.rotation);
+					break;
+				case 1:
+					Instantiate (platformCorner,spawnerN.position,spawnerN.rotation);
+					break;
+				}
+				break;
+			case 1:
+				switch(choice)
+				{
+				case 0:
+					Instantiate (platformStraight,spawnerE.position,spawnerE.rotation);
+					break;
+				case 1:
+					Instantiate (platformCorner,spawnerE.position,spawnerE.rotation);
+					break;
+				}
+				break;
+			case 2:
+				switch(choice)
+				{
+				case 0:
+					Instantiate (platformStraight,spawnerW.position,spawnerW.rotation);
+					break;
+				case 1:
+					Instantiate (platformCorner,spawnerW.position,spawnerW.rotation);
+					break;
+				}
+				break;
+			case 3:
+				switch(choice)
+				{
+				case 0:
+					Instantiate (platformStraight,spawnerW.position,spawnerS.rotation);
+					break;
+				case 1:
+					Instantiate (platformCorner,spawnerW.position,spawnerS.rotation);
+					break;
+				}
+				break;
+			}
+			
+			
+		}
+	}
+}
