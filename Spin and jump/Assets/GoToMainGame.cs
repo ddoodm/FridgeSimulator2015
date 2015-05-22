c75b551817694fd656fd0a0768a02aba81cbe3ac
robafett <ram_95@msn.com>
2015-05-22 21:21:15 +1000

    Made the Tutorial send you to the main game

---
+++ Spin and jump/Assets/GoToMainGame.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToMainGame : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
